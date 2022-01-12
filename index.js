9da03305a36359f0b5da05c176fa5e042c17fd08
= <rtyenriques@gmail.com>
2022-01-12 00:38:19 -0800

    commit tree e26ec55f70fec4b153bcc99667b06ae21aacaefd

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    // return string.replace(/[^A-Za-z0-9-'])+/g,'');
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+
+  }
+  static titleize(string){
+    // return string.toUpperCase(/[^a anbut of andforatbyfrom]+/g, '')
+    let newS = string.split(' ');
+    let k = newS.map(function(a) {
+      return a.capitalize
+    })
+    return k.join()
+  }
 }
